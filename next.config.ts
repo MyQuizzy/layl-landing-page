a85e9cb17e34c402156008669b52f298998e629c
JaafarRammal <jarammal@gmail.com>
2025-11-25 15:27:00 +0000

    add path

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/layl-landing-page" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/layl-landing-page" : "",
 };
 
 export default nextConfig;
